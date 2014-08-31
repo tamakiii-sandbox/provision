f72144f7bb7c78114c91d2fb8fcbf66fd49295bb
Daichi Tamaki <tamaki@retriever.lan>
2014-09-01 02:29:45 +0900

    httpd.confのtemplateを用意

--- chef/site-cookbooks/apache/recipes/default.rb
+++ chef/site-cookbooks/apache/recipes/default.rb
@@ -6,6 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package 'httpd' do
-  action :install
+
+["httpd", "httpd-devel", "mod_ssl"].each do |pkg|
+  package "#{pkg}" do
+    action :install
+  end
+end
+
+service 'httpd' do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start]
+end
+
+template "/etc/httpd/conf/httpd.conf" do
+  source "httpd-2.2.conf.erb"
 end
