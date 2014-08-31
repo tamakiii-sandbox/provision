bcbfde7d3a961c4093622546d992150d5bbafab6
Daichi Tamaki <tamaki@retriever.lan>
2014-09-01 03:59:47 +0900

    Berksでzshを導入
    * $ bin/berks vendor vendor/cookbooks

--- chef/.chef/knife.rb
+++ chef/.chef/knife.rb
@@ -5,4 +5,4 @@ environment_path "environments"
 data_bag_path    "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
-knife[:berkshelf_path] = "cookbooks"
+knife[:berkshelf_path] = "../vendor/cookbooks"
