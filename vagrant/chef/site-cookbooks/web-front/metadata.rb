65beea5e148bebdfa82e3acb05be02efab2418d4
d-tamaki <daichi_tamaki@voyagegroup.com>
2014-09-09 13:00:16 +0900

    depends漏れを追加

--- vagrant/chef/site-cookbooks/web-front/metadata.rb
+++ vagrant/chef/site-cookbooks/web-front/metadata.rb
@@ -6,4 +6,6 @@ description      'Installs/Configures web-front'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'override-apache'
+depends 'override-mysql'
 depends 'override-php'
