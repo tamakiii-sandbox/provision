e9d5d5718c70e19b9d171af124949cd80ac34d89
d-tamaki <daichi_tamaki@voyagegroup.com>
2014-09-09 12:59:43 +0900

    depends apache -> apache2

--- vagrant/chef/site-cookbooks/override-apache/metadata.rb
+++ vagrant/chef/site-cookbooks/override-apache/metadata.rb
@@ -6,4 +6,5 @@ description      'Installs/Configures override-apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apache'
+depends "apache2"
+
