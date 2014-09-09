460ced5dcebd14bbd5fe330b659537375ddf4d58
d-tamaki <daichi_tamaki@voyagegroup.com>
2014-09-09 13:00:49 +0900

    apache2をベースにoverride-apacheを修正

--- vagrant/chef/site-cookbooks/override-apache/recipes/default.rb
+++ vagrant/chef/site-cookbooks/override-apache/recipes/default.rb
@@ -7,11 +7,45 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "httpd" do
-  action :install
+include_recipe 'apache2'
+
+# disable default site
+apache_site '000-default' do
+  enable false
 end
 
-service "httpd" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable , :start ]
+# create directory
+directory '/web/httpd' do
+  action :create
+  recursive true
 end
+
+# Set up config
+web_app "vuejs" do
+  docroot "/web/httpd/vuejs/public"
+  template "vuejs.conf.erb"
+  server_name node[:fqdn]
+  server_aliases [node[:hostname], "vuejs"]
+end
+
+# create apache config
+template "#{node['apache']['dir']}/sites-available/vuejs.conf" do
+  source 'apache2.conf.erb'
+  notifies :restart, 'service[apache2]'
+end
+
+template 'apache2.conf' do
+  path "#{node['apache']['conf_dir']}/httpd.conf"
+  action :create
+  source 'apache2.conf.erb'
+  owner 'root'
+  group node['apache']['root_group']
+  mode '0644'
+  notifies :reload, 'service[apache2]', :delayed
+end
+
+# enable
+apache_site 'vuejs.conf' do
+  enable true
+end
+
