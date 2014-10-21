c4b20f70b108c8690b6af9fac85002bd945b718c
Daichi Tamaki <tamaki.htm@gmail.com>
2014-10-22 04:46:29 +0900

    add chsh recipe

---
+++ vagrant/private/chef/site-cookbooks/chsh/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: chsh
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+bash "Set vagrant shell to zsh" do
+    code <<-EOT
+        chsh -s /bin/zsh vagrant
+    EOT
+    not_if 'test "/bin/zsh" = "$(grep vagrant /etc/passwd | cut -d: -f7)"'
+end
